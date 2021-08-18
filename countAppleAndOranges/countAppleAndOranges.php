8508aa17a4a244027a8622df5c574e565848a7c0
Elisha Ukpong <ishukpong418@gmail.com>
2021-08-18 23:38:24 +0100

    added code solution to apples and oranges problem

--- countAppleAndOranges/countAppleAndOranges.php
+++ countAppleAndOranges/countAppleAndOranges.php
@@ -1,1 +1,23 @@
 <?php
+
+function countApplesAndOranges($s, $t, $a, $b, $apples, $oranges) {
+
+    $orangeCount = 0;
+    $appleCount = 0;
+
+    array_map(function($apple) use ($s,$t,$a, &$appleCount){
+        $apple += $a;
+
+        if($apple >= $s && $apple <= $t) $appleCount++;
+    }, $apples);
+
+    array_map(function($orange) use ($s,$t,$b, &$orangeCount){
+        $orange +=$b;
+
+        if($orange >= $s && $orange <= $t) $orangeCount++;
+    }, $oranges);
+
+
+    echo $appleCount . " \n";
+    echo $orangeCount . " \n";
+}
